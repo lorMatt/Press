33b07513052629158489797d25fc71dad6f4f40d
Lorenzo Mattioli <lorenzomattioli01@gmail.com>
2025-02-25 16:16:06 +0100

    Restart, press scrapers

---
+++ Scraper CorrUmbria.R
@@ -0,0 +1,6 @@
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+p_load(tidyverse, rvest)
+
+# Define link ------------------------------------------------------------------
+link <- 
